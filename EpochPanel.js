3b347e4f0872f5764d1f5b024659e5fb68bf847f
Tom Lieber <tom@alltom.com>
2013-06-28 13:35:22 -0700

    epoch panel reuses DOM nodes instead of recreating them, fixing the hover & click issues

--- EpochPanel.js
+++ EpochPanel.js
@@ -30,14 +30,12 @@ define(function (require, exports, module) {
     var PanelManager = brackets.getModule("view/PanelManager");
 
     var $exports = $(exports);
-    var $panel;
-    var $eventsContainer, $events;
-    var panel; // PanelManager.Panel
 
-    var epochHandle;
-    var hitsByAgent = {}; // { agent-id: { name: hits } }
-    var combinedHits = {}; // { name: hits }
-    var eventNameDisplayOrder; // event name display order
+    var panel; // PanelManager.Panel
+    var $panel;
+    var $exceptionsContainer, $exceptionCount,
+        $logsContainer, $logCount,
+        $allEventsContainer, $eventContainers = {}, $eventCounts = {}; // last two are { name: $dom }
 
     var exceptionHandle;
     var exceptionCountByAgent = {}; // { agent-id: count }
@@ -47,23 +45,25 @@ define(function (require, exports, module) {
     var consoleLogsCountByAgent = {}; // { agent-id: count }
     var combinedConsoleLogsCount = 0;
 
-    function DisplayOrder() {
-        this.objects = [];
-    }
-    DisplayOrder.prototype = {
-        add: function (object) {
-            if (this.objects.indexOf(object) === -1) {
-                this.objects.push(object);
-            }
-        },
-        sort: function (objects) {
-            return objects.sort(function (a, b) {
-                return this.objects.indexOf(a) - this.objects.indexOf(b);
-            }.bind(this));
-        },
+    var epochHandle;
+    var hitsByAgent = {}; // { agent-id: { name: hits } }
+    var combinedHits = {}; // { name: hits }
+
+    function combineExceptions() {
+        combinedExceptionCount = 0;
+        for (var agentId in exceptionCountByAgent) {
+            combinedExceptionCount += exceptionCountByAgent[agentId];
+        }
     }
 
-    function combineData() {
+    function combineConsoleLogs() {
+        combinedConsoleLogsCount = 0;
+        for (var agentId in consoleLogsCountByAgent) {
+            combinedConsoleLogsCount += consoleLogsCountByAgent[agentId];
+        }
+    }
+
+    function combineHits() {
         combinedHits = {};
         for (var agentId in hitsByAgent) {
             var hits = hitsByAgent[agentId];
@@ -71,28 +71,6 @@ define(function (require, exports, module) {
                 combinedHits[name] = (combinedHits[name] || 0) + hits[name];
             }
         }
-
-        combinedExceptionCount = 0;
-        for (var agentId in exceptionCountByAgent) {
-            combinedExceptionCount += exceptionCountByAgent[agentId];
-        }
-
-        combinedConsoleLogsCount = 0;
-        for (var agentId in consoleLogsCountByAgent) {
-            combinedConsoleLogsCount += consoleLogsCountByAgent[agentId];
-        }
-    }
-
-    function hitsReceived(agent, hits) {
-        if (!(agent.id in hitsByAgent)) {
-            hitsByAgent[agent.id] = {};
-        }
-        var agentHits = hitsByAgent[agent.id];
-        for (var name in hits) {
-            agentHits[name] = (agentHits[name] || 0) + hits[name].hits;
-            eventNameDisplayOrder.add(name);
-        }
-        combineData();
     }
 
     function exceptionsReceived(agent, hits) {
@@ -102,7 +80,10 @@ define(function (require, exports, module) {
         for (var nodeId in hits) {
             exceptionCountByAgent[agent.id] += hits[nodeId];
         }
-        combineData();
+        combineExceptions();
+
+        updateExceptionDisplay();
+        setPanelVisibility();
     }
 
     function consoleLogsReceived(agent, count) {
@@ -110,32 +91,42 @@ define(function (require, exports, module) {
             consoleLogsCountByAgent[agent.id] = 0;
         }
         consoleLogsCountByAgent[agent.id] = count;
-        combineData();
+        combineConsoleLogs();
+
+        updateConsoleLogsDisplay();
+        setPanelVisibility();
+    }
+
+    function hitsReceived(agent, hits) {
+        if (!(agent.id in hitsByAgent)) {
+            hitsByAgent[agent.id] = {};
+        }
+        var agentHits = hitsByAgent[agent.id];
+        for (var name in hits) {
+            agentHits[name] = (agentHits[name] || 0) + hits[name].hits;
+        }
+        combineHits();
+
+        updateEventsDisplay();
+        setPanelVisibility();
     }
 
     function agentLeft(agent) {
-        delete hitsByAgent[agent.id];
         delete exceptionCountByAgent[agent.id];
         delete consoleLogsCountByAgent[agent.id];
-        combineData();
+        delete hitsByAgent[agent.id];
+        combineExceptions();
+        combineConsoleLogs();
+        combineHits();
+
+        updateAllDisplays();
+        setPanelVisibility();
     }
 
-    function eventDom(name, hits) {
-        var $dom = $("<span class='epoch' />");
-        var $nameDom = $("<span class='name' />").text(name).appendTo($dom);
-        var $hitsDom = $("<span class='hits' />").text(hits).appendTo($dom);
-
-        $dom.on("click", function () {
-            $exports.triggerHandler("eventNameClicked", [name]);
-        });
-
-        return $dom;
-    }
-
-    function exceptionDom(count) {
+    function exceptionDom() {
         var $dom = $("<span class='epoch exception' />");
         var $nameDom = $("<span class='name' />").text("exception").appendTo($dom);
-        var $hitsDom = $("<span class='hits' />").text(count).appendTo($dom);
+        $exceptionCount = $("<span class='hits' />").appendTo($dom);
 
         $dom.on("click", function () {
             $exports.triggerHandler("exceptionsClicked", [name]);
@@ -144,10 +135,10 @@ define(function (require, exports, module) {
         return $dom;
     }
 
-    function consoleLogDom(count) {
+    function consoleLogDom() {
         var $dom = $("<span class='epoch logs' />");
         var $nameDom = $("<span class='name' />").text("console.log").appendTo($dom);
-        var $hitsDom = $("<span class='hits' />").text(count).appendTo($dom);
+        $logCount = $("<span class='hits' />").appendTo($dom);
 
         $dom.on("click", function () {
             $exports.triggerHandler("logsClicked", [name]);
@@ -156,40 +147,64 @@ define(function (require, exports, module) {
         return $dom;
     }
 
-    function _haveEventData() { return Object.keys(combinedHits).length > 0 }
-    function _haveExceptionData() { return combinedExceptionCount > 0 }
-    function _haveConsoleLogsData() { return combinedConsoleLogsCount > 0 }
-    function _haveData() {
-        return _haveEventData() || _haveExceptionData() || _haveConsoleLogsData();
+    function eventDom(name) {
+        var $dom = $("<span class='epoch' />");
+        var $nameDom = $("<span class='name' />").text(name).appendTo($dom);
+        var $hitsDom = $("<span class='hits' />").appendTo($dom);
+
+        $dom.on("click", function () {
+            $exports.triggerHandler("eventNameClicked", [name]);
+        });
+
+        return { container: $dom, count: $hitsDom };
     }
 
-    function display() {
-        $events.empty();
+    function _haveExceptionData() { return combinedExceptionCount > 0 }
+    function _haveConsoleLogsData() { return combinedConsoleLogsCount > 0 }
+    function _haveEventData() { return Object.keys(combinedHits).length > 0 }
+    function _haveData() {
+        return _haveExceptionData() || _haveConsoleLogsData() || _haveEventData();
+    }
 
-        if (_haveData()) {
-            panel.show();
+    function updateExceptionDisplay() {
+        $exceptionCount.text(combinedExceptionCount);
+        $exceptionsContainer.toggle(_haveExceptionData());
+    }
 
-            if (_haveExceptionData()) {
-                $events.append(exceptionDom(combinedExceptionCount));
+    function updateConsoleLogsDisplay() {
+        $logCount.text(combinedConsoleLogsCount);
+        $logsContainer.toggle(_haveConsoleLogsData());
+    }
+
+    function updateEventsDisplay() {
+        for (var name in combinedHits) {
+            if (!(name in $eventContainers)) {
+                var o = eventDom(name);
+                $eventContainers[name] = o.container;
+                $eventCounts[name] = o.count;
+                $allEventsContainer.append($eventContainers[name]);
             }
+            $eventCounts[name].text(combinedHits[name]);
+        }
 
-            if (_haveConsoleLogsData()) {
-                $events.append(consoleLogDom(combinedConsoleLogsCount));
+        // delete DOMs for events that no longer exist
+        for (name in $eventContainers) {
+            if (!(name in combinedHits)) {
+                $eventContainers[name].remove();
+                delete $eventContainers[name];
+                delete $eventCounts[name];
             }
-
-            if (_haveEventData()) {
-                var sortedNames = eventNameDisplayOrder.sort(Object.keys(combinedHits));
-                sortedNames.forEach(function (name) {
-                    $events.append(eventDom(name, combinedHits[name]));
-                });
-            }
-        } else {
-            panel.hide();
         }
     }
 
-    function hasEvent(name) {
-        return (name in combinedHits);
+    function updateAllDisplays() {
+        updateExceptionDisplay();
+        updateConsoleLogsDisplay();
+        updateEventsDisplay();
+    }
+
+    function setPanelVisibility() {
+        panel.setVisible(_haveData());
     }
 
     function hasExceptions() {
@@ -200,49 +215,46 @@ define(function (require, exports, module) {
         return combinedConsoleLogsCount > 0;
     }
 
-    function init() {
-        eventNameDisplayOrder = new DisplayOrder;
+    function hasEvent(name) {
+        return (name in combinedHits);
+    }
 
+    function init() {
         $panel = $("<div id='theseus-epoch-panel' class='bottom-panel no-focus' />");
         panel = PanelManager.createBottomPanel("theseus.epoch-panel", $panel, 20);
 
-        $eventsContainer = $("<span />").appendTo($panel);
-        $("<span class='heading' />").text("Events:").appendTo($eventsContainer);
-        $events = $("<span class='events' />").appendTo($eventsContainer);
-
-        epochHandle = AgentHandle.trackEpochs(100);
-        $(epochHandle).on("data", function (ev, data) {
-            hitsReceived(data.agent, data.data);
-            display();
-        });
-        $(epochHandle).on("agentDisconnected", function (ev, agent) {
-            agentLeft(agent);
-            display();
-        });
+        $("<span class='heading' />").text("Events:").appendTo($panel);
+        $allEventsContainer = $("<span class='events' />").appendTo($panel);
+        $exceptionsContainer = $("<span />").appendTo($allEventsContainer).append(exceptionDom()).hide();
+        $logsContainer = $("<span />").appendTo($allEventsContainer).append(consoleLogDom()).hide();
 
         exceptionHandle = AgentHandle.trackExceptions(100);
         $(exceptionHandle).on("data", function (ev, data) {
             exceptionsReceived(data.agent, data.data.counts);
-            display();
         });
         $(exceptionHandle).on("agentDisconnected", function (ev, agent) {
             agentLeft(agent);
-            display();
         });
 
         consoleLogsHandle = AgentHandle.trackConsoleLogs(100);
         $(consoleLogsHandle).on("data", function (ev, data) {
             consoleLogsReceived(data.agent, data.data);
-            display();
         });
         $(consoleLogsHandle).on("agentDisconnected", function (ev, agent) {
             agentLeft(agent);
-            display();
+        });
+
+        epochHandle = AgentHandle.trackEpochs(100);
+        $(epochHandle).on("data", function (ev, data) {
+            hitsReceived(data.agent, data.data);
+        });
+        $(epochHandle).on("agentDisconnected", function (ev, agent) {
+            agentLeft(agent);
         });
     }
 
     exports.init = init;
-    exports.hasEvent = hasEvent;
     exports.hasExceptions = hasExceptions;
     exports.hasLogs = hasLogs;
+    exports.hasEvent = hasEvent;
 });
