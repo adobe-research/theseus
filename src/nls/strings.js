5c240c0945612802c9b40424b94cee301dd7acb2
Carlos Santos <carlos.santos@innabler.co.uk>
2014-11-12 10:57:58 +0000

    Add portuguese from Portugal translation

--- src/nls/strings.js
+++ src/nls/strings.js
@@ -28,12 +28,13 @@
 define(function (require, exports, module) {
     // Code that needs to display user strings should call require("strings") to load
     // strings.js. This file will dynamically load strings.js for the locale specified by brackets.locale.
-    // 
+    //
     // Translations for other locales should be placed in nls/<locale<optional country code>>/strings.js
     // Localization is provided via the i18n plugin.
     // All other bundles for languages need to add a prefix to the exports below so i18n can find them.
     module.exports = {
         root: true,
         de: true,
+        pt: true
     };
 });
